ba8627f15c67926d8f2b382ba96b7a76ed23abba
Jessica Hille <jessicalhille@gmail.com>
2021-11-04 22:30:32 -0500

    added if statements and for statement

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-const lowercaseOptions = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numberOptions = "0123456789";
-const specialOptions = "!@#$%^&*()_-+=;:/?.,<>"
+var lowercaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialOptions = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 function generatePassword() {
   var passwordLength = window.prompt("How many characters would you like your password to contain?"); 
@@ -13,40 +13,42 @@ function generatePassword() {
     if (passwordLength >= 8 && passwordLength <= 128) {
       console.log(passwordLength)
     } else {
-      window.alert("You must type a number between 8 and 128! Please try again.")
+      window.alert("You must type a number between 8 and 128! Please try again.");
       return generatePassword();
     }
 
-  var lowercase = confirm("Click OK to include lowercase characters.")
-    if (lowercase === true) {
-      console.log(lowercase);
+  var lowercase = confirm("Click OK to include lowercase characters.");
+  var uppercase = confirm("Click OK to include uppercase characters.");
+  var numericChar = confirm("Click OK to include numeric characters.");
+  var specialChar = confirm("Click OK to include special characters.");
+
+    if (lowercase === false && uppercase === false && numericChar === false && specialChar === false) {
+      window.alert("You must choose at least one option! Please try again.");
+      return generatePassword();
     }
 
-  var uppercase = confirm("Click OK to include uppercase characters.")
-    if (uppercase === true) {
-      console.log(uppercase);
-    }
-  
-  var numericChar = confirm("Click OK to include numeric characters.")
-    if (numericChar === true) {
-      console.log(numericChar);
-    }
+  var passwordCharacters = []
 
-  var specialChar = confirm("Click OK to include special characters.")
-    if (specialChar === true) {
-      console.log(specialChar);
-    }
+  if (lowercase) {
+    passwordCharacters = passwordCharacters.concat(lowercaseOptions)
+  }
+  if (uppercase) {
+    passwordCharacters = passwordCharacters.concat(uppercaseOptions)
+  }
+  if (numberOptions) {
+    passwordCharacters = passwordCharacters.concat(numberOptions)
+  }
+  if (specialOptions) {
+    passwordCharacters = passwordCharacters.concat(specialOptions)
+  }
 
-  // b. lowercase, uppercase, numeric, special characters
+  var password = ""
 
-// 2. validate the input
+  for (var i = 0; i < passwordLength; i++) {
+    password = password + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  }
 
-// 3. generate password based on criteria
-
-// 4. display generated password on the page
-
-
-  return "password here";
+  return password;
 } 
 
 
