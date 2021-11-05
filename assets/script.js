cba00ebfe0d787d79e4cac7a0e000c5d09ca3e6c
Jessica Hille <jessicalhille@gmail.com>
2021-11-04 22:03:35 -0500

    hard reset, back to basics

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,54 @@
 // Assignment code here
-var lowercaseOptions = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberOptions = "0123456789";
-var specialOptions = "!@#$%^&*()_-+=;:/?.,<>"
+const lowercaseOptions = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberOptions = "0123456789";
+const specialOptions = "!@#$%^&*()_-+=;:/?.,<>"
 
-function getLength() {
+function generatePassword() {
   var passwordLength = window.prompt("How many characters would you like your password to contain?"); 
     if (passwordLength === "" || passwordLength === null) {
       window.alert("You need to provide a valid number! Please try again.");
-      return getLength();
+      return generatePassword();
     }
     if (passwordLength >= 8 && passwordLength <= 128) {
       console.log(passwordLength)
     } else {
       window.alert("You must type a number between 8 and 128! Please try again.")
-      return getLength();
+      return generatePassword();
     }
-};
+
+  var lowercase = confirm("Click OK to include lowercase characters.")
+    if (lowercase === true) {
+      console.log(lowercase);
+    }
+
+  var uppercase = confirm("Click OK to include uppercase characters.")
+    if (uppercase === true) {
+      console.log(uppercase);
+    }
+  
+  var numericChar = confirm("Click OK to include numeric characters.")
+    if (numericChar === true) {
+      console.log(numericChar);
+    }
+
+  var specialChar = confirm("Click OK to include special characters.")
+    if (specialChar === true) {
+      console.log(specialChar);
+    }
 
   // b. lowercase, uppercase, numeric, special characters
-function getLowercase() {
-  var lowercaseLetters = window.prompt("Click OK to confirm including lowercase letters.");
-    if (lowercaseLetters === true) {
-      return lowercaseLetters;
-    } else (lowercaseLetters === false) {
-      return lowercaseLetters;
-    }
-};
-
-function getUppercase() {
-  var uppercaseLetters = window.prompt("Click OK to confirm including uppercase letters.");
-    if (uppercaseLetters === true) {
-      return uppercaseLetters;
-    } else (uppercaseLetters === false) {
-      return uppercaseLetters;
-    };
-};
-
-function getNumeric() {
-  var numericCharacters = window.prompt("Click OK to confirm including numberic characters.");
-    if (numericCharacters === true) {
-      return numericCharacters;
-    } else {};
-};
-
-  var specialCharacters = window.prompt("Click OK to confirm including special characters.");
-    if (specialCharacters) {
-      console.log(specialCharacters);
-    } else {};
 
 // 2. validate the input
 
 // 3. generate password based on criteria
 
-
-
 // 4. display generated password on the page
-  return "generated password will go here";
-}
+
+
+  return "password here";
+} 
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
